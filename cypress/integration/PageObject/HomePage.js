be1df2ddfea21d67f84f005a2f5cce0e28d2faf7
Jeffery Brown <usagimadokii@gmail.com>
2022-06-16 14:17:26 -0500

    removed comment

--- cypress/integration/PageObject/HomePage.js
+++ cypress/integration/PageObject/HomePage.js
@@ -1,26 +1,3 @@
-/* class HomePage {
-    visitHomePage(){
-        cy.visit('https://google.com')
-    }
-    clickHomeSearch(){
-        cy.get('div[class="RNNXgb"]').click() //Search Bar
-    }
-    googleSearchGenerator(NoSubmit) {
-        const myText = ["I have always wanted ","You'll never catch me alive facebook!",
-            "Lorem Lipsum I cant speak latin", "My music tastes might need some improving.", "I hate bing"] //The text cypress will enter into the text field
-        const randomNumber = Math.floor(Math.random() * myText.length) //Choose a random number between 0 and the # of phrases in myText
-        cy.get('div[class="RNNXgb"]').type(myText[randomNumber]); //takes a random text and types it into the search field
-        if(!NoSubmit) {
-            cy.get('form').submit(); //submit the form
-        }
-    }
-    clickSearchSuggestion(){
-        cy.get('div[class="aajZCb"]').click() //Click on search suggestion
-    }
-}
-export default HomePage
- */
-
 class HomePage {
     visitHomePage(){
         cy.visit('https://okcrovercentral.com')
